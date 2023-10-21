ed6d2851093463677b3d4859d450696b8d213d2a
Brie Morris <morris.brie@gmail.com>
2023-10-21 10:35:35 -0500

    back button to home on info

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -8,14 +9,22 @@ import { useHistory } from 'react-router-dom';
 // or even care what the redux state is
 
 function InfoPage() {
- 
+  const store = useSelector((store) => store);
+  const history = useHistory();
+  const [heading, setHeading] = useState('Coding info on app add button on home screen');
+
+  const toHome = (event) => {
+    history.push('/login')
+  }
+  
 
 
 
   return (
     <div className="container">
-      <h1>Coding info on app add button on home screen</h1>
-      
+      <h1>{heading}</h1>
+      <br/>
+      <button onClick={toHome} className="btn">Back</button>
     </div>
   );
 }
