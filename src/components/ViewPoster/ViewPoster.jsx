7d5a539211b29c576d27aecc13c9baa09fdc8910
Brie Morris <morris.brie@gmail.com>
2023-10-27 12:06:46 -0500

    still working on view content

--- src/components/ViewPoster/ViewPoster.jsx
+++ src/components/ViewPoster/ViewPoster.jsx
@@ -15,16 +15,36 @@ function ViewPoster(posters) {
  
   const history = useHistory();
   const [heading, setHeading] = useState('Queers have hopefully added images and memories to this event.');
+  const [thisPoster, setThisPoster] = useState(
+    {id: 0, 
+    poster_img: '',
+    description: '',
+    date: 0,
+    memory: '',
+    images: ''
+  } );
+
   const dispatch = useDispatch();
   const poster = useSelector(store => store.posterReducer.viewPosterContent);
   console.log('POSTER', poster);
+  console.log('POSTER', poster[0]);
 
   const {id} = useParams();
   console.log('use params', id);
+
+  
   
   useEffect(() => {
     dispatch({ type: 'VIEW_POSTER', payload: id });
+    setThisPoster({id: poster[0].poster_id , 
+      poster_img: poster[0].poster_img,
+      description: poster[0].description,
+      date: poster[0].date,
+      memory: poster[0].memory,
+      images: poster[0].images
+    } )
 }, []);
+
   
   const toArchive = (event) => {
     history.push('/archive')
@@ -37,13 +57,13 @@ function ViewPoster(posters) {
       <h2>{heading}</h2>
       <br /> <br />
 
-        <div key={poster.id}>
-          <img src={poster.poster_img} alt={poster.picture} />
-          <h2>{poster.description}</h2>
-          <h3>{poster.date}</h3>
+        <div key={thisPoster.id}>
+          <img src={thisPoster.poster_img} alt={thisPoster.picture} />
+          <h2>{thisPoster.description}</h2>
+          <h3>{thisPoster.date}</h3>
           <br /><br />
-          <h2>{poster.memory}</h2>
-          <img src={poster.images} alt={poster.pictures} />
+          <h2>{thisPoster.memory}</h2>
+          <img src={thisPoster.images} alt={thisPoster.pictures} />
         </div>
   
 
