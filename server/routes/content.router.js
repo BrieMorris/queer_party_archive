5aedec368a043460190a059ad9bbcb0515febec7
Brie Morris <morris.brie@gmail.com>
2023-10-28 13:56:39 -0500

    post in content router

--- server/routes/content.router.js
+++ server/routes/content.router.js
@@ -3,26 +3,27 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 /**
- * GET route for all posters appearing in the Archive
+ * GET - i don't think I need this
  */
-router.get('/', (req, res) => {
-  const queryText = `SELECT `
-  pool.query(queryText)
-   .then(result => {
-    res.send(result.rows);
-   })
-   .catch(err => {
-    console.log('ERROR; Get get poster img');
-    res.sendStatus(500)
-   })
-});
+// router.get('/', (req, res) => {
+//   const queryText = `SELECT `
+//   pool.query(queryText)
+//    .then(result => {
+//     res.send(result.rows);
+//    })
+//    .catch(err => {
+//     console.log('ERROR; Get get poster img');
+//     res.sendStatus(500)
+//    })
+// });
 
 /**
  * POST 
  */
 router.post('/', (req, res) => {
   console.log(req.body);
-  const addContent = `INSERT INTO
+  const addContent = `INSERT INTO "posters" ("user_id", "poster_img", "description", "date")
+  VALUES ($1, $2, $3, $4)
   `
 });
 
