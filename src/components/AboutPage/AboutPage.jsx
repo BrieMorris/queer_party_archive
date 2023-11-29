5d25b9ce16eb3bbd147598117ec65b3eb394563a
Brie Morris <morris.brie@gmail.com>
2023-11-29 13:57:15 -0600

    removing code I do not need

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+// // This is one of our simplest components
+// // It doesn't have local state,
+// // It doesn't dispatch any redux actions or display any part of redux state
+// // or even care what the redux state is'
 
-function AboutPage() {
-  return (
-    <div className="container">
-      <div>
-        <h1>I think I can get rid of this page </h1>
-      </div>
-    </div>
-  );
-}
+// function AboutPage() {
+//   return (
+//     <div className="container">
+//       <div>
+//         <h1>I think I can get rid of this page </h1>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default AboutPage;
+// export default AboutPage;
--- src/components/MemorialMap/MemorialMap.jsx
+++ src/components/MemorialMap/MemorialMap.jsx
@@ -4,12 +4,8 @@ import { useHistory } from 'react-router-dom';
 import IMG_pedra from './IMG_pedra.jpg'
 import IMG_marcella1 from './IMG_marcella1.jpg'
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
+//
 function MemorialMap(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState('(Future Goal) Memorial Map: Queer spaces from our past.');
   const history = useHistory();
